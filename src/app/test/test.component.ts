ea892ec55047e95fbb896b49ef8f9e7a097a84e2
Natzun <natanzun@outlook.com.br>
2022-09-06 23:46:33 -0300

    Atualização Início
    Conhecendo Angular.

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { AppComponent } from '../app.component';
 
@@ -9,15 +10,36 @@ import { AppComponent } from '../app.component';
 export class TestComponent implements OnInit {
   title = 'test';
 
+  test: any;
+
   constructor(
     private cdr: ChangeDetectorRef,
     private app: AppComponent
   ) {
     console.log('[TestComponent#constructor]');
+
+    this.test = [
+      'dudu',
+      'shy',
+      'natan'
+    ];
   }
 
   ngOnInit(): void {
     console.log('[TestComponent#ngOnInit]');
     this.app.updateView(this.title);
+  }
+
+  testando(){
+    alert('Cuidado!');
+  }
+
+  alertar(msg:string) {
+    alert(msg);
+  }
+
+  naoClique(){
+    this.app.redirectTo('delete');
+    alert('Falei pra não clicar!');
   }
 }
